3694339a83548fb33b1e802c6d19d0d83020e5b9
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2021-10-19 12:13:08 +0200

    update README.md and dependencies versions

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# hotello:keycloak-oauth
+# eole:keycloak-oauth
+
+forked from hotello:keycloak-oauth
+https://github.com/hotello/meteor-accounts-auth0.
 
 An implementation of the Keycloak OAuth flow. See the [Meteor Guide](https://guide.meteor.com/accounts.html) for more details.
 
-Automatically imported by hotello:accounts-keycloak.
+Automatically imported by eole:accounts-keycloak.
--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: 'hotello:keycloak-oauth',
+  name: 'eole:keycloak-oauth',
   summary: 'Keycloak OAuth flow',
-  version: '2.1.0',
-  git: 'https://github.com/hotello/meteor-accounts-auth0.git'
+  version: '2.2.0',
+  git: 'https://github.com/eole/meteor-accounts-auth0.git',
 });
 
 Npm.depends({
-  'keycloak-connect': '4.5.0'
+  'keycloak-connect': '4.5.0',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.5');
+Package.onUse(function (api) {
+  api.versionsFrom(['1.5', '2.3']);
 
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
